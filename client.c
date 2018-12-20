73f56564cdb436d2d24653cac90358b2f426b689
Taaseen Ali <taaseen.ali89@gmail.com>
2018-12-19 20:50:44 -0500

    done

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 #include "pipe_networking.h"
-
+#include <stdio.h>
 
 int main() {
 
@@ -7,4 +7,14 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+  
+  char string[BUFFER_SIZE];
+  char response[BUFFER_SIZE];
+  while(1){
+    printf("[CLIENT]: ");
+    fgets(string, BUFFER_SIZE, stdin);
+    write(to_server, string, BUFFER_SIZE);
+    read(from_server, response, BUFFER_SIZE);
+    printf("[SERVER] %s", response);
+  }
 }
